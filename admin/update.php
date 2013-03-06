d2ac1bb00c25a45036c04fe0212bdb00e2fab38c
Jennifer <Lsealaa@aol.com>
2013-03-06 15:56:46 -0500

    working on form updating proper page information

--- admin/update.php
+++ admin/update.php
@@ -1,9 +1,22 @@
 <?php
 	require_once('../inc/config.php');
+	
+	//catch user selection from dropdown menu and sanitize end of url so no one can add more to it and mess it up
+	$tmp = $_GET['p'];
+	
+	if($tmp === 'home' || $tmp === 'about' || $tmp === 'gallery')
+	{
+		$page = $tmp;
+	}
+	else
+	{
+		$page = 'home';
+	}
+	
 	//get all content related to this page (home)
 	$sql="SELECT * 
 	FROM site_content 
-	WHERE page_name='home' 
+	WHERE page_name='$page' 
 	AND section_name='blurb'";
 	$myData=$db->query($sql);
 	
@@ -19,11 +32,11 @@
 	if(isset($_POST['submitted']))
 	{
 		$user_content=mysqli_real_escape_string($db, $_POST['body']);
-		
+		$page = $_POST['tmp'];
 		
 		$sql="UPDATE site_content
 		SET content='$user_content'
-		WHERE page_name='home'
+		WHERE page_name='$page'
 		AND section_name='blurb'";
 		
 		$myData=$db->query($sql);
@@ -51,11 +64,12 @@
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     	<fieldset>
         <legend>Update Page Info</legend>
+        <input type="hidden" id="tmp" name="tmp" value="<?php echo $page ?>" />
         <select name="page" id="page" onchange="set_page(this)">
-        	<option value="index.php">Home</option>
-            <option value="about.php">About</option>
-            <option value="contact.php">Contact</option>
-        </select>
+        	<option value="home">Home</option>
+            <option value="about">About</option>
+            <option value="gallery">Gallery</option>
+        </select>   
         <label for="body">Body</label>
             <textarea name="body" rows="10" cols="30">
             <?php echo $main; ?>
